fe8b557d6f305ac89c4d2b7dcad1c69c2a16f94d
ERUD1T3 <josiasmoukpe@gmail.com>
2019-03-28 18:56:42 -0400

    started on hw6

---
+++ hw/hw6/graph.h
@@ -0,0 +1,13 @@
+/************************************
+ * GRAPH INTERFACE FOR HW6
+ * Josias Moukpe
+ ************************************/ 
+
+
+
+#ifndef GRAPH_H
+#define GRAPH_H 
+
+
+
+#endif // GRAPH_H
--- hw/hw6/hw6.c
+++ hw/hw6/hw6.c
@@ -12,22 +12,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "sllist.h"
 
-/*
-  Description of the function, parameter, and return value (if any)
- */
 
-int main(int argc, char* argv[])
+
+int main(int argc, char** argv)
 {
-  /*
-    description of declared variables
-   */
 
-
+ FILE* fp = fopen(argv[1], "r"); // pointer the input data and query files
   
-  /*
-    description of each "block" (about 5 lines of instructions)
-   */
+  if(fp == NULL) { // exits with failure if program fails to open data file
+    printf("\nError: failed to open input file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Reading file inputs
+  size_t len = 0; // keeps track of the length of the lines
+  char* input_line = NULL;  // the input line charracter array  
+  SLList* queries = NULL; 
+  
+  // loop while not the end of the input file
+  while(getline(&input_line, &len, fp) != EOF) {
+    queries = parseWords(input_line); // converts input line into sllist of words
+    // processQuery(logs, queries); // process the sllist of words in cmds
+  }
+  
+  // destroySList(logs);
+  fclose(fp);
+  return EXIT_SUCCESS;
   
 }
 
